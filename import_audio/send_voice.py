718639438cd9e46289090cf3bee39e3ca123b40b
bobbyskywalker <aleksander.garbacz@gmail.com>
2025-02-22 16:49:48 +0100

    dupa

--- import_audio/send_voice.py
+++ import_audio/send_voice.py
@@ -1,7 +1,8 @@
 import os
 from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
+
+# Set the environment variable for the Google credentials
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "cloud_key.json"  # Ensure the file name is correct
 
 def transcribe_audio(file_path):
     client = speech.SpeechClient()
@@ -9,18 +10,20 @@ def transcribe_audio(file_path):
     with open(file_path, 'rb') as audio_file:
         content = audio_file.read()
 
-    audio = types.RecognitionAudio(content=content)
+    audio = speech.RecognitionAudio(content=content)
 
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,  # Change this to match the sample rate of your audio file
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,  # Use FLAC encoding
+        sample_rate_hertz=48000,  # Adjusted sample rate to 48000 Hz
         language_code='en-US',    # Specify the language of the audio
+        audio_channel_count=2     # Set to 2 for stereo audio
     )
 
     response = client.recognize(config=config, audio=audio)
 
     for result in response.results:
-        print("Transcript: {}".format(result.alternatives[0].transcript))
+        print("{}".format(result.alternatives[0].transcript))
 
-audio_file_path = "path/to/your/audio/file.wav"
+# Make sure the audio file is in the same directory
+audio_file_path = "miss_south_carolina.flac"
 transcribe_audio(audio_file_path)
